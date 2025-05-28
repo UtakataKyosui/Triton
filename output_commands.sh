7c8140b8ea93df0d73db745255bd5bdae0f6a4da
Tiamat-KIT <fill.ayaextech@gmail.com>
2025-05-28 19:57:17 +0900

    docs: リリースノートの使い方わからない

--- output_commands.sh
+++
@@ -1,9 +0,0 @@
-cargo loco generate scaffold User string:string string:string string:date_time string:date_time
-cargo loco generate scaffold Post string:string string:text string:references string:string string:date_time string:date_time
-cargo loco generate scaffold Category string:string string:string string:date_time string:date_time
-cargo loco generate scaffold Tag string:string string:string string:date_time string:date_time
-cargo loco generate scaffold Comment string:text string:references string:references string:date_time string:date_time
-cargo loco generate scaffold o
-cargo loco generate scaffold o
-cargo loco generate scaffold o
-cargo loco generate scaffold o
